e7cc84bdea6fb77634b8b200f1fab2065372a06f
mathildetho <61235108+mathildetho@users.noreply.github.com>
2020-03-26 18:31:25 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React from "react";
-import Contact from "./components/Contact";
-import PropTypes from "prop-types";
+import ContactList from "./components/ContactList";
 
 function App() {
   return (
     <div className="App">
-      <Contact
-        online
-        avatar="https://randomuser.me/api/portraits/men/12.jpg"
-        name="Craig Anderson"
-      />
-      <Contact
-        offline
-        avatar="https://randomuser.me/api/portraits/men/85.jpg"
-        name="Peter Palmer"
-      />
-      <Contact
-        online
-        avatar="https://randomuser.me/api/portraits/women/94.jpg"
-        name="Pamela Dixon"
-      />
+      <ContactList />
     </div>
   );
-}
-
-Contact.propTypes = {
-  online: PropTypes.bool.isRequired,
-  avatar: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired
 };
 
 export default App;
